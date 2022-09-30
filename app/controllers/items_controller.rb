c8a69e26ee902a38c61de551a69abbbc16624f3b
Ken Muyesu <ken.muyesu@student.moringaschool.com>
2022-09-30 15:19:45 +0300

    commit tree 813b7e268a0e1023f025a7edc104ed5180911878

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,39 @@
 class ItemsController < ApplicationController
+rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
 
   def index
-    items = Item.all
+    if params[:user_id]
+      user = find_by_user_id
+      items = user.items
+    else
+      items = Item.all
+    end
     render json: items, include: :user
+  end
+
+  def show
+    item = Item.find(params[:id])
+    render json: item, include: :user
+  end
+
+  def create # creating for a specific user
+    user = find_by_user_id
+    item = user.items.create!(item_params)
+    render json: item, status: :created
+  end
+
+  private
+  # refactoring
+  def find_by_user_id
+    User.find(params[:user_id])
+  end
+
+  def item_params
+    params.permit(:name, :description, :price)
+  end
+
+  def render_not_found_response
+    render json: { error: "Items Not Found" }, status: :not_found
   end
 
 end
